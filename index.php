eda4be11cf24b0902f5c5910ef82e77ba151f64b
God Mod <godmod23@web.de>
2014-12-21 00:51:12 +0100

    Test

--- index.php
+++ index.php
@@ -252,9 +252,6 @@ function check_if_repo_in_eqdkp(){
 bootstrap();
 ?>
 
-
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 	<head>
